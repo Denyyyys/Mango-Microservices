1c41e4b8664c5973d81e42608e85985dd6377666
Denyyyys <denys.fokashchuk@gmail.com>
2024-02-28 20:46:00 +0100

    first commit

---
+++ Mango.Services.CouponAPI/Data/AppdbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppdbContext : DbContext
+    {
+        public AppdbContext(DbContextOptions<AppdbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }   
+}
